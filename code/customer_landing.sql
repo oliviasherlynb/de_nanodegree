1c33a3c393a8db4eeb6f8b7871dc63f71320923b
oliviasherlynb <146457122+oliviasherlynb@users.noreply.github.com>
2023-12-11 17:52:57 +0000

    Create customer_landing.sql

---
+++ code/customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `os-stedi-database`.`customer_landing` (
+  `customerName` string,
+  `email` string,
+  `phone` string,
+  `birthDay` string,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `lastUpdateDate` bigint,
+  `shareWithResearchAsOfDate` bigint,
+  `shareWithPublicAsOfDate` bigint,
+  `shareWithFriendsAsOfDate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'FALSE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://os-stedi-lakehouse/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
