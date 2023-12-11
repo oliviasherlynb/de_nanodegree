c4c85430aef38b224ac165b743d9f8fb27e35398
oliviasherlynb <146457122+oliviasherlynb@users.noreply.github.com>
2023-12-11 17:53:33 +0000

    Create accelerometer_landing.sql

---
+++ code/accelerometer_landing.sql
@@ -0,0 +1,17 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `os-stedi-database`.`accelerometer_landing` (
+  `user` string,
+  `timestamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'FALSE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://os-stedi-lakehouse/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json');
